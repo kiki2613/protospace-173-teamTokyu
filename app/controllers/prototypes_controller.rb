dac1e93c463c5ee1bb28d1d808546dcc7d636088
kiki2613 <keywnm@gmail.com>
2024-06-07 17:01:32 +0900

    Active Storageの導入

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -18,6 +18,6 @@ class PrototypesController < ApplicationController
   private
 
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept)
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
   end
 end
