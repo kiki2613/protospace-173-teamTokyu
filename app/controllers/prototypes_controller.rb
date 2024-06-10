0946de380680bf5cbd2789c48bf3e9d3377fc68d
kiki2613 <keywnm@gmail.com>
2024-06-10 14:28:56 +0900

    投稿機能実装（ユーザーなし）

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+# before_action :move_to_signed_in, expect: :index
+
   def index
     @prototypes = Prototype.all
   end
@@ -22,4 +24,10 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image)
   end
+
+  # def move_to_signed_in
+  #   unless user_signed_in?
+  #     redirect_to action: :index
+  #   end
+  # end
 end
