a88a0a3e01c66d8afd9870bd7526910f9aea0784
kiki2613 <keywnm@gmail.com>
2024-06-12 12:34:58 +0900

    prototypesコントローラーとアソシエーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :prototype
-  has_many :comment
+  has_many :comments
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
